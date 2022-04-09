4af6c443a00127cc0b1df9a5e0ebf800c9327900
Debonet <719893090@qq.com>
2022-04-10 02:14:01 +0800

    feat: fetch paifu information

---
+++ README.md
@@ -0,0 +1,33 @@
+## About
+
+Fetch m-league paifu information.
+
+## Usage
+
+- sign in [https://m-league.jp/](https://m-league.jp/)
+
+- "日程・対戦成績" => choose any match day => "牌譜をみる"
+
+- check site cookies, copy paifuSessionId and enter it in the .env file.
+
+-
+
+```shell
+npm install
+npm start
+```
+
+or
+
+```shell
+yarn
+yarn start
+```
+
+## Other
+
+Tested environment:
+
+node: v16.14.0
+
+operation system: Windows 10
---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { parse } from "node-html-parser";
+import sleep from "./utils/sleep";
+import { writeFile } from "fs";
+import path from "path";
+import { config } from "dotenv";
+
+const paifuSessionId = config().parsed?.cookie;
+if (!paifuSessionId) {
+  throw "Please enter your paifuSessionId in .env file, for more information, please check README.md.";
+}
+
+const SEASON_LIST = [
+  "games/2018-season",
+  "games/2019-season",
+  "games/2020-season",
+  // current season (2021)
+  "games",
+];
+const BASE_URL = "https://m-league.jp";
+const GAME_URL = "https://viewer.ml-log.jp/web/viewer";
+const GAME_DATA_REG = /UMP_PLAYER\.init\(true, true, '(.+)', autoplay\);/;
+const REQUEST_GAP = 500; // The frequency of requests is about REQUEST_GAP/SEASON_LIST.length (ms)
+
+// set timeout as 30s for every request
+const req = axios.create({
+  timeout: 30000,
+});
+
+// multiple seasons in parallel requests, for one season, every match(半荘) is in sequence
+for (const season of SEASON_LIST) {
+  console.time(season);
+  req
+    .get<string>(`${BASE_URL}/${season}`)
+    .then(async (res) => {
+      const seasonPage = parse(res.data);
+      const gameIdList = seasonPage
+        .querySelectorAll(".js-viewer-form")
+        .map((item) => item.getAttribute("data-game-id"));
+      for (const gameId of gameIdList) {
+        if (!gameId) continue;
+        const gameInfo = await req.get<string>(`${GAME_URL}?gameid=${gameId}`, {
+          headers: {
+            Cookie: `paifuSessionId=${paifuSessionId}`,
+          },
+        });
+        const regMatches = gameInfo.data.match(GAME_DATA_REG);
+        if (regMatches) {
+          writeFile(
+            path.resolve(__dirname, `../data/${gameId}.json`),
+            regMatches[1],
+            { encoding: "utf-8" },
+            () => {
+              console.log(`fetch information about ${gameId} finished.`);
+            }
+          );
+        }
+        await sleep(REQUEST_GAP);
+      }
+      console.timeLog(season, `fetch games info successfully.`);
+    })
+    .catch((e) => {
+      console.timeLog(season, `error occurs while fetching games`, e);
+    });
+}
